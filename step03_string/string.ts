21bff25f3a5e1c14eab8b5330cfb2781780f1aae
mhassandata <mhassandata@gmail.com>
2024-03-17 21:58:14 +0500

    Udated the step03 folder

--- step03_string/string.ts
+++ step03_string/string.ts
@@ -3,3 +3,14 @@ let string_message = "A person who never made a mistake never tried anything new
 let author_name = "Albert Einstein"
 
 console.log(author_name + " once said " +string_message)
+
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// let famous_person = "Steve Jobs"
+// let quotation = "Everyone in this country should learn how to program because it teaches you how to think"
+
+// console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+
+let famous_person = 'Steve Jobs';
+let message1  = `${famous_person} once said, "Everyone in this country should learn how to program because it teaches you how to think."`;
+console.log(message1);
