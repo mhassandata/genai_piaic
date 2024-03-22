c0787648f40fe4d8fc48a30d1d74a8d6cea8b625
mhassandata <mhassandata@gmail.com>
2024-03-22 21:41:41 +0500

    udated and completed the step05

--- step05_operation/operations.ts
+++ step05_operation/operations.ts
@@ -1,3 +1,20 @@
+// #Simple way
+// Addition
+console.log("5 + 3 =", 5 + 3);
+
+// Subtraction
+console.log("10 - 2 =", 10 - 2);
+
+// Multiplication
+console.log("2 * 4 =", 2 * 4);
+
+// Division
+console.log("16 / 2 =", 16 / 2);
+
+
+
+// with the help of variables
+
 let num1 = 5;
 let num2 = 8;
 let num3 = 9;
