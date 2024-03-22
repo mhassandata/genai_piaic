171b21982c4f7743a01f9dc1030b3a348474a84c
mhassandata <mhassandata@gmail.com>
2024-03-22 23:19:41 +0500

    Updated the step07

--- step07_storarray/friendsNames.js
+++ step07_storarray/friendsNames.js
@@ -3,3 +3,6 @@ const names = ['Hassan', 'Zahid', 'Asad', 'Eid'];
 for (const name of names) {
     console.log(name);
 }
+for (const name of names) {
+    console.log(`This is the name of person ${name}.`);
+}
--- step07_storarray/friendsNames.ts
+++ step07_storarray/friendsNames.ts
@@ -3,3 +3,13 @@ const names: string[] = ['Hassan', 'Zahid', 'Asad', 'Eid'];
 for (const name of names){
     console.log(name)
 }
+
+
+// Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, but each message should be personalized with the person’s name.//
+
+for (const name of names){
+    console.log(`This is the name of person ${name}.`);
+}
+
