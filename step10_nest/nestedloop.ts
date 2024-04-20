bb6743bcf4b7536ae20a934f7c44cd3c70a7fdcb
mhassandata <mhassandata@gmail.com>
2024-04-20 15:34:04 +0500

    Added step10 and step11

---
+++ step10_nest/nestedloop.ts
@@ -0,0 +1,15 @@
+let country = 'pakistn'
+
+
+let age =10
+
+if (country == 'pakistan'){
+    if(age > 18) {
+        console.log('your are elibal for vote')
+    }
+else {
+    console.log('You are not eligibal for vote')
+}
+
+
+}
---
+++ step11_functions/funtion.ts
@@ -0,0 +1,7 @@
+function greet(name:string){
+    console.log("Hello" +name)
+}
+
+
+//parameters, augruments 
+// zero parametres 
