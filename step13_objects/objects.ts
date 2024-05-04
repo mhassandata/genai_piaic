9471bea7c41333a4a20b718ae1bb849260a2dc59
mhassandata <mhassandata@gmail.com>
2024-05-04 17:05:59 +0500

    Updated the object file

--- step13_objects/objects.ts
+++ step13_objects/objects.ts
@@ -65,5 +65,27 @@ let tube1: Tube = {
 ball1 = sphere1
 sphere1 = ball1
 
-tube1 = ball1   //not equal  because tube1 has diameter and length but ball1 has only diameter
-ball1 = tube1 //both are equal
+// tube1 = ball1   //not equal  because tube1 has diameter and length but ball1 has only diameter
+ball1 = tube1 //both are equal becuase ball1 has diameter which is also in tube1
+
+
+/// Duck typing
+let myType = {name: "Hassan", id: 1}; // duck typing
+
+//anonymous typing
+let myType:{name:string, id:number, age?:number} = {name:'hassan', id: 011, age:28}
+
+//type alias
+// let myType ={name:string, id:number}
+
+
+//After all these limitation then introduce Interfaces
+
+interface MyType {
+    name: string
+    age: number
+}
+let myType2: MyType = {
+    name: "Hassan",
+    age: 29
+}
